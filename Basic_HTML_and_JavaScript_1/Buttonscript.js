e2cbca22b6ca7e18368df24b5e34230d63352a64
Andrei <atrubaa@gmail.com>
2023-02-12 11:23:37 -0800

    Changed button color

--- Basic_HTML_and_JavaScript_1/Buttonscript.js
+++ Basic_HTML_and_JavaScript_1/Buttonscript.js
@@ -1,4 +1,4 @@
 function My_First_Function(){
   var String = "Kiss me, I'm Irish!";
-  var result = String.fontcolor("green");
+  var result = String.fontcolor("blue");
   document.getElementById("Irish").innerHTML = result;}
