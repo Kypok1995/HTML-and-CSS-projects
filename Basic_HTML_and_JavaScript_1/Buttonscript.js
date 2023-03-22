4ae7fc3053a8fb52683af5aad5d286acc9cfca18
Andrei <atrubaa@gmail.com>
2023-03-22 14:50:55 -0700

    add slideshow function

--- Basic_HTML_and_JavaScript_1/Buttonscript.js
+++ Basic_HTML_and_JavaScript_1/Buttonscript.js
@@ -2,3 +2,14 @@ function My_First_Function(){
   var String = "Kiss me, I'm Irish!";
   var result = String.fontcolor("blue");
   document.getElementById("Irish").innerHTML = result;}
+
+  $("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
