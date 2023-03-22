9de4d0edba1bec90773a51fb276e066f2497c98e
Andrei <atrubaa@gmail.com>
2023-03-22 11:49:28 -0700

    add a popup form

--- One_page_website/JS/one_page_website.js
+++ One_page_website/JS/one_page_website.js
@@ -44,3 +44,10 @@ window.addEventListener('DOMContentLoaded', () => {
   document.body.classList.remove('fade-out');
 });
 
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
